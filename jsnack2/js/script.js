0912fd51e30357d5a8b9d5fb9d8d166129229f81
albertoferri <falberto99@gmail.com>
2024-02-05 14:46:24 +0100

    snack 2    impostate variabili

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -3,3 +3,10 @@
 // Il programma poi deve far comparire un messaggio "il numero è troppo alto" oppure "il numero è troppo basso" 
 // nel momento in cui il numero detto dall'utente non è perfettamente uguale al numero estratto in modo casuale.
 // Quando l'utente trova il numero, fateglielo sapere!
+
+// Genera un numero casuale tra 1 e 100 dai indovinare
+const numeroSegreto = Math.floor(Math.random() * 100) + 1;
+console.log("numero da indovinare: " + numeroSegreto);
+
+// setto variabile tentativi = 0 per tenere traccia dei tentativi effettuati
+let tentativi = 0;
