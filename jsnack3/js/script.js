6efa05435d339e3351255df0bce7f35bdc384998
albertoferri <falberto99@gmail.com>
2024-02-05 15:22:10 +0100

    snack 3   inserisco i due array

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,2 +1,6 @@
 // Snack 3
 // Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+// creo due array con un numero diverso di elementi
+const array1 = [1, 2, 3, 4];
+const array2 = [5, 6, 7, 8, 9, 10, 11, 12];
