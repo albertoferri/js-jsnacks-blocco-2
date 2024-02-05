6fa01054c674f665f231e20aad4953a8419e9dd2
albertoferri <falberto99@gmail.com>
2024-02-05 15:09:40 +0100

    snack 4 finito

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -6,3 +6,13 @@ const userNumber = Number( prompt("Inserisci un numero:"));
 
 // stampo in pagina il titolo
 document.getElementById("title").innerText = "ODD-EVEN GAME";
+
+
+// Verifica se il numero è pari o dispari
+if (userNumber % 2 == 0) {
+    // Se è pari, stampa il numero
+    document.getElementById("num").innerHTML = ("Il numero inserito è pari: " + userNumber);
+} else {
+    // Se è dispari, stampa il numero successivo
+    document.getElementById("num").innerHTML = ("Il numero inserito è dispari. Il numero successivo è: " + (userNumber + 1));
+}
