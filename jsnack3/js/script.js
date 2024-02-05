b257e9bac6d7cc5222c232fb8adb70793e6fbd7d
albertoferri <falberto99@gmail.com>
2024-02-05 15:35:34 +0100

    inserito ciclo while e print in console

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -2,5 +2,20 @@
 // Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 
 // creo due array con un numero diverso di elementi
-const array1 = [1, 2, 3, 4];
-const array2 = [5, 6, 7, 8, 9, 10, 11, 12];
+const firstArray = [1, 2, 3, 4];
+const secondArray = [5, 6, 7, 8, 9, 10, 11, 12];
+
+// Verifica quale array è più corto
+while (firstArray.length !== secondArray.length) {
+
+    const userInput = Number(prompt("inserisci un numero"));
+
+    if (firstArray.length < secondArray.length) {
+        firstArray.push(userInput);
+    } else {
+        secondArray.push(userInput);
+    }
+}
+
+console.log("Array 1:", firstArray);
+console.log("Array 2:", secondArray);
