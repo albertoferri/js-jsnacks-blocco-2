a3ebce6f0a54b5ffec2effc31316386c593e1340
albertoferri <falberto99@gmail.com>
2024-02-05 15:57:09 +0100

    snack 3    finito

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -2,8 +2,8 @@
 // Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 
 // creo due array con un numero diverso di elementi
-const firstArray = [1, 2, 3, 4];
-const secondArray = [5, 6, 7, 8, 9, 10, 11, 12];
+const firstArray = [100, 200, 300, 400];
+const secondArray = [500, 600, 700, 800, 900, 1000, 1100, 1200];
 
 // Verifica quale array è più corto
 while (firstArray.length !== secondArray.length) {
@@ -16,6 +16,7 @@ while (firstArray.length !== secondArray.length) {
         secondArray.push(userInput);
     }
 }
-
-console.log("Array 1:", firstArray);
-console.log("Array 2:", secondArray);
+// STAMPO IN PAGINA
+document.getElementById("title").innerText = "ARRAY GAME"
+document.getElementById("array1").innerHTML = "Il primo array: " + firstArray;
+document.getElementById("array2").innerHTML = "Il secondo array: " + secondArray;
