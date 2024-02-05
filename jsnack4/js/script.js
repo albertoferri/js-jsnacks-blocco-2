0ddbc00ca7c5908d12ed890c1d884e31b70982cb
albertoferri <falberto99@gmail.com>
2024-02-05 14:56:32 +0100

    inserito variabili e print in pagina del titolo

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,2 +1,8 @@
 // Snack 4
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+// Richiedi all'utente di inserire un numero tramite prompt
+const userNumber = Number( prompt("Inserisci un numero:"));
+
+// stampo in pagina il titolo
+document.getElementById("title").innerText = "ODD-EVEN GAME";
